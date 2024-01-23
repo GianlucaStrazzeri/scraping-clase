1b26c0760f366ee388463c02fa56482848b128e0
GianlucaStrazzeri <githubandwebprojects@gmail.com>
2024-01-23 08:39:07 +0100

    Añadidendo comentar para entender el codigo

--- Explicacionproject.js
+++ Explicacionproject.js
@@ -1,21 +1,54 @@
 //scraping o automatización es el uso de datos web para recuperar datos para un analisis , seguimiento de precios
-//librerias: popiteir cheerio
+//librerias: poopiteer cheerio
+
 //              Terminal
-//paso 1 npm init -y //inicializo npm en la terminal con -y le estoy dando que yes a todo
-//paso 2 npm i express axios cheerio -E //instalo librerias  en la terminal on -E quito el caret
+//paso 1 npm init -y //inicializo npm con -y le estoy dando que yes a todo
+//paso 2 npm i express axios cheerio -E //instalo librerias con -E quito el caret
+
+//          index.js
 //paso 3 voy a generar nuestro servidor html const express=require("express");
-//paso 4 requiero cheerio y axios
+//paso 4 const cheerio = require('cheerio');//requiero cheerio
+//const axios=require("axios");//requiero axios
 //paso 5 voy a escuchar el servidor con app.listen Min 4:06
+
+//      package.json
 //paso 6 voy a ir al package.json para crear un "start": "node --watch index.js" todo eso va en scripts
-//paso 7 en la terminal voy y le doy a npm start para que inicie el watch y haga los cambios automaticos MIn:4:52
-//paso 8 creo una comstante con la url a la que voy a acceder
-//paso 9 Vamos a utilizar axios para scrapear y le vamos a decir donde queremos que ponga la información
-//paso 10 voy a hacer un app.get para que ponga esa información y un axios.get para traernos la url
+
+//              Terminal
+//paso 7  npm start para que inicie el watch y haga los cambios automaticos MIn:4:52
+
+//          index.js
+//paso 8 creo una constante con la url a la que voy a acceder const url="https://carlosdiazgirol.github.io/dashboard/"
+//paso 9 Vamos a utilizar axios para scrapear y le vamos a decir donde queremos que
+// ponga la información con un app.get y un axios.get para traernos la url
 //esto es como un fetch por lo tanto tendré que utilizar el .then para pasar la respuesta
 //el const html es la constante que guarda el contenido de la url Min 8.52
 //imprimo con console.log html
+
+//              Terminal
 //esto en la terminal me devuelve todo el codigo fuente html de la pagina que quiero scrapear Min 9:05
-//ahora vamos a recoger parte de la estructura y debajo de console.log(html) pongo un res.send(html)
-//Esto me permite reproducir la estructura en mi localhost3000 del html de la pagina web que esté rscrapeando MIn 9:46
+
+//          index.js
+//paso 10 ahora vamos a recoger parte de la estructura y debajo de console.log(html) pongo un res.send(html)
+//Esto me permite reproducir la estructura en mi localhost3000 del html de la pagina web que esté
+// escrapeando MIn 9:46
 //también puedo acceder solo a partes de la pagina web para restructurar la pagina voy a hacerlo
-//con cheerio Min:11:OO
+//con cheerio  voy a comentar // console.log(html)  // res.send(html) para modificarlo con cheerio Min:11:OO 
+//llamo a cheerio creando una const $=cheerio.load(html) el dolar se utilizaba para utilizar una libreria de jquery muy
+//buena para recorrer el DOM , cherio.load dice de cargame lo que me vas a traer
+//const pageTitle=$("title").text(); lo que va a hacer es crear una constante =a cheerio que traerá
+//el titulo en formato texto MIn:12:15
+//Lo imprimo en local host haciendo res.send(`<h1>${pageTitle}</h1>`) 
+//si ahora quiero sacarme todos los enlaces y las imagenes de la pagina voy a usar una función de cheerio
+//que es similar al foreach pero en lugar de ser un for each es un each
+//const links=[]; creo una constante links donde voy a poner un array con los enlaces
+//$("a").each((index,element)=>{})
+//Un for Each recorre primero el elemento y luego el indice y el cheerio .each funciona al revés
+//con el cheerio.each  recorremo el indice primero y luego el elemento
+//const link=$(element).attr("href")   recorro los link con el attributo href Min:15:00
+//links.push(link) subo los enlaces al array vacio que había creado
+
+
+
+//              Terminal
+//En la terminal me imprime ahora todos los enlaces del proyecto
--- index.js
+++ index.js
@@ -1,20 +1,48 @@
+//Pasos previos en la terminal 
+//inicializo npm con -y le estoy dando que yes a todo
+//npm init -y
+//instalo librerias con -E quito el caret
+//npm i express axios cheerio -E
+//En el package.json en scripts creo un "start": "node --watch index.js" para que se actualice
+//de forma automatica el servidor y en la terminal lo inicializo con npm start
+
 const cheerio = require('cheerio');//requiero cheerio
 const axios=require("axios");//requiero axios
-const express=require("express");//requiero express
+const express=require("express");//requiero express y genero un servidor html
 const app=express()//inicializo express
 
-const url="https://carlosdiazgirol.github.io/dashboard/"
+const url="https://carlosdiazgirol.github.io/dashboard/" //constante que define la url
 
-app.get("/", (req,res)=> {
-axios.get(url).then((response)=>{
-    if (response.status === 200){
-        const html=response.data
-        console.log(html)
-        res.send(html)
+app.get("/", (req,res)=> {//Pon la siguiente información en la ruta de la home
+axios.get(url).then((response)=>{ //requiere información con axios y luego...
+    if (response.status === 200){//si el estado de la respuesta es positivo ===200
+        const html=response.data//crea una constante html =contenido del html
+       // console.log(html)   //imprimo en la terminal todo el html del documento a escrapear  
+        // res.send(html)    //envio al localHost toda la estructura del documento a escrapear
+       const $= cheerio.load(html) //se define cheerio con un $ porque antes se utilizaba ese simbolo en una 
+       //libreria que se llama jquery para modificar el DOM, .load() quiere decir cargame lo que te vaya a decir
+       const pageTitle=$("title").text();//creo una constante=cheerio(titulo delapagina) .text() indica que lo saco en formato texto
+
+       const links=[];//creo una constante vacía donde meteré los enlaces de la pagina 
+
+$("a").each((index,element)=>{//cheerio tiene una forma suya de hacer bucles forEach el cheerio.each 
+    //como argumento se han puesto los "a" funciona al reves que for Each, aquí se itera el index y luego el elemento
+    //con lo que como argumento tengo que poner index aunque luego no lo utilice
+    const link=$(element).attr("href")//le digo que la constante link (al singular) es = a los 
+    //elementos de cheerio cuyo attributo utilizando .attr() sea igual a "href", luego pusheo el
+    // resultado a la constante links (al plural) que esta fuera de esta función
+    links.push(link)
+})
+
+//imprimo en la consola los enlaces
+console.log(links) 
+//envio al localHost el titulo de la pagina que estoy escrapeando
+       res.send(`<h1>${pageTitle}</h1>`)
     }
 })
 })
 
-app.listen(3000,()=>{
+//Escucho el servidor
+app.listen(3000,()=>{ 
     console.log("express esta escuchando en el puerto http://localhost:3000")
 })
